4691266d11d3df2f24ba56aa2e473de13c12cbfe
Jose Leon <jl3184502@gmail.com>
2025-06-04 08:02:33 -0600

    Un solo manejador

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
-const buttonMaquina = document.querySelector('#maquina');
-const buttonOtroJugador = document.querySelector('#otroJugador');
 const container = document.querySelector('.container');
 
-buttonMaquina.addEventListener('click', function () {
-    container.classList.add('containerSalida');
+function crearPantallaPrincipal(){
+    container.classList.add('pantallaPrincipalAnimation');
 
-    setTimeout(function () {
-        container.innerHTML = `
-            <h2>Nuevo contenido</h2>
-            <p>Este es un párrafo</p>
-        `;
-        container.classList.remove('containerSalida');
-        container.classList.add('containerEnter');
-    }, 1000); 
+    setTimeout(function(){
+        container.innerHTML = ``;
+        container.insertAdjacentHTML('beforeend', `
+            <figure class="container__figure">
+                <img src="img/ahorcado_0.png" alt="ahorcado">
+            </figure>
+            <div class="container__buttons">
+                <button id="maquina">Maquina</button>
+                <button id="otroJugador">Otro jugador</button>
+            </div>
+        `)
+        container.classList.remove('pantallaPrincipalAnimation')
+    }, 1000)
+}
 
-    setTimeout(function () {
-        container.classList.remove('containerEnter');
-    }, 2000);
-});
+container.addEventListener('click', function(e){
+    const elemto = e.target
+
+    if(elemto.matches('button')){
+        let html
+        if(elemto.matches('#maquina')){
+            html = `
+            <h2>Vas a jugar vs maquina</h2>
+            <p>parrafo</p>
+            `
+        }else{
+            html = `
+            <h2>Vas a jugar vs otro jugador</h2>
+            <p>parrafo 2</p>
+            `
+        }
+
+        container.classList.add('containerSalida');
+
+        setTimeout(function () {
+            container.innerHTML = ``;
+            container.insertAdjacentHTML('beforeend', html)
+            container.classList.remove('containerSalida');
+            container.classList.add('containerEnter');
+        }, 1000); 
+
+        setTimeout(function () {
+            container.classList.remove('containerEnter');
+        }, 2000);
+
+    }
+})
